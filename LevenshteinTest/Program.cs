d3a56b74bea6b2e42af9233789b09fad8ebea078
JBelles <JBelles@IRDITJBELLES>
2018-08-21 12:06:09 -0700

    Initial

--- LevenshteinTest/Program.cs
+++ LevenshteinTest/Program.cs
@@ -8,90 +8,92 @@ namespace LevenshteinTest
     {
         public static void Main()
         {
-            List<char[]> dictionary = new List<char[]>(){ "name1".ToCharArray(), "bryce".ToCharArray(), "kevin".ToCharArray(), "dorian".ToCharArray()};
+            List<char[]> dictionary = new List<char[]>() { "word".ToCharArray(), "word1".ToCharArray(), "ward".ToCharArray(), "weird".ToCharArray(), "olfw".ToCharArray(), "asdasdqweqweasdasdqwe123123".ToCharArray() };
+            char[] target = "wolf".ToCharArray();
 
-            int[,] chart = new int[dictionary.Count(), dictionary.Count()];
-            double[] avg = new double[dictionary.Count()];
-
+            (char[], int)[] chart = new (char[], int)[dictionary.Count()];
             Array.Clear(chart, 0, chart.Length);
-            for (int i = 0; i < dictionary.Count(); i++)
+
+            for (int j = 0; j < dictionary.Count(); j++)
             {
-                for(int j = 0; j < dictionary.Count(); j++)
-                {
-                    if (i != j)
-                    {
-                        var distance = LevenshteinDistance(dictionary[i], dictionary[j]);
-                        chart[i, j] = distance;
-                        if (j == 0)
-                        avg[i] += distance;
-                    }
-                }
+                var distance = LevenshteinDistance(target, dictionary[j]);
+                chart[j] = (dictionary[j], distance);
             }
-            for(int i = 0; i < avg.Length; i++)
-            {
-                avg[i] /= dictionary.Count();
-            }
-            PrintNames(dictionary);
+            Console.WriteLine($"Comparing to target word: {new string(target)}");
             PrintChart(chart);
             Console.ReadLine();
         }
-        public static void PrintNames(List<char[]> Records)
-        {
-            foreach(char[] record in Records)
-            {
-                Console.Write("[" + new string(record) + "] ");
-            }
-            Console.WriteLine();
-        }
-        public static void PrintChart(int[,] chart)
-        {
 
-            for (int i = 0; i < chart.GetLength(0); i++)
-            {
-                for (int j = 0; j < chart.GetLength(0); j++)
-                {
-                    Console.Write($"[   {chart[i, j]}   ]");
-                }
-                Console.WriteLine();
-            }
-        }
         public static int LevenshteinDistance(char[] m, char[] n)
         {
-            if(m.Length == n.Length)
-            {
-                return HammingDistance(m, n);
-            }
-            int[,] matrix = new int[m.Length+1, n.Length+1];
+            //I originally thought "If the strings are equal length, just use Hamming Distance since it's more efficient"
+
+            /*Consider 'mann' and 'annm', Hamming distance would be 3 since there are 3 char pairs that don't match,
+              but Levenshtein would show only 2, since 'mann' -> 'ann' -> 'annm' */
+
+            //if(m.Length == n.Length)
+            //{
+            //    return HammingDistance(m, n);
+            //}
+
+            //instantiate a new matrix [m+1, n+1]
+            int[,] matrix = new int[m.Length + 1, n.Length + 1];
+            //zero fill the matrix
             Array.Clear(matrix, 0, matrix.Length);
 
+            //first column of the X axis compute the Distance of m from an empty string
+            /* X   m a n n
+             *   0 1 2 3 4
+             * a -------->
+             * n  
+             * n  
+             * m  
+             */
             for (int i = 0; i < m.Length + 1; i++)
             {
                 matrix[i, 0] = i;
             }
 
+            //first column of the Y axis compute the Distance of n from an empty string
+            /* X   m a n n
+             *   0 1 2 3 4
+             * a 1 |
+             * n 2 |
+             * n 3 |
+             * m 4 V
+             */
             for (int j = 0; j < n.Length + 1; j++)
             {
                 matrix[0, j] = j;
             }
 
-            for (int j = 1; j < n.Length+1; j++)
+            for (int j = 1; j < n.Length + 1; j++)
             {
-
-                for (int i = 1; i < m.Length+1; i++)
+                //move accross and down rows 
+                for (int i = 1; i < m.Length + 1; i++)
                 {
                     int substitutionCost = 0;
 
-                    if (m[i-1] != n[j-1])
+                    if (m[i - 1] != n[j - 1])
                     {
                         substitutionCost = 1;
                     }
-                    var min = (matrix[i - 1, j - 1] + substitutionCost);
-                    min = min < matrix[i - 1, j] + 1 ? min : matrix[i - 1, j] + 1;
-                    min = min < matrix[i, j - 1] + 1 ? min : matrix[i, j - 1] + 1;
+                    var min = (matrix[i - 1, j - 1] + substitutionCost); //substitution
+                    min = min < matrix[i - 1, j] + 1 ? min : matrix[i - 1, j] + 1; //deletion
+                    min = min < matrix[i, j - 1] + 1 ? min : matrix[i, j - 1] + 1; //insertion
                     matrix[i, j] = min;
                 }
             }
+            //This would look something like:
+            /* X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |X   m a n n |
+             *   0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |  0 1 2 3 4 |
+             * a 1 1------>|a 1 1 1---->|a 1 1 1 2-->|a 1 1 1 2 3 |a 1 1 1 2 3 |a 1 1 1 2 3 |a 1 1 1 2 3 |a 1 1 1 2 3 |a 1 1 1 2 3 |a 1 1 1 2 3 |a 1 1 1 2 3 |a 1 1 1 2 3 |a 1 1 1 2 3 |a 1 1 1 2 3 |a 1 1 1 2 3 |a 1 1 1 2 3 |
+             * n 2         |n 2         |n 2         |n 2         |n 2 2------>|n 2 2 2---->|n 2 2 2 1-->|n 2 2 2 1 1 |n 2 2 2 1 1 |n 2 2 2 1 1 |n 2 2 2 1 1 |n 2 2 2 1 1 |n 2 2 2 1 1 |n 2 2 2 1 1 |n 2 2 2 1 1 |n 2 2 2 1 1 |
+             * n 3         |n 3         |n 3         |n 3         |n 3         |n 3         |n 3         |n 3         |n 3 3------>|n 3 3 3---->|n 3 3 3 1-->|n 3 3 3 1 1 |n 3 3 3 1 1 |n 3 3 3 1 1 |n 3 3 3 1 1 |n 3 3 3 1 1 |
+             * m 4         |m 4         |m 4         |m 4         |m 4         |m 4         |m 4         |m 4         |m 4         |m 4         |m 4         |m 4         |m 4 3------>|m 4 3 4---->|m 4 3 4 2-->|m 4 3 4 2 2 |
+             */
 
+            //Last value is what we're after!
             return matrix[m.Length, n.Length];
         }
 
@@ -110,5 +112,14 @@ namespace LevenshteinTest
             }
             return sum;
         }
+        public static void PrintChart((char[], int)[] chart)
+        {
+
+            for (int i = 0; i < chart.Length; i++)
+            {
+                Console.WriteLine($"{new string(chart[i].Item1)}: {chart[i].Item2}");
+            }
+        }
+      
     }
 }
